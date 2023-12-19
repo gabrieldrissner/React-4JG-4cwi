12bd64c195d289589aaaf2d80dd03093ecb1cea0
Gabriel <gabriel.drissner@student.htldornbirn.at>
2023-12-19 09:29:51 +0100

    christmas-page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Person from "./christmas/Person";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React nnnnnnfdsa 
-        </a>
-      </header>
-    </div>
+    <div className="main" style={{backgroundImage:"url('bild4.jpg')"}}>
+      <Person name="Weihnachten" description="24.12.2023" />
+      <Person name="Meine Wunschliste" description="von Samuel Meusburger" />
+      <Button name="Sam will" age="eine Kiste Kölbö" />
+      <Button name="Sam will" age="An oasar i Mathe" />
+      <Button name="Sam will" age=" einen Leberkäs Semmel mit Ketchup Majo" />
+      <Button name="Sam will" age=" eine Rote Ampel" />
+      </div>
+
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import styles from "./Button.module.css";
+
+export default function Button(props) {
+  const [isOn, setIsOn] = useState();
+
+  return (
+    <div
+      className={isOn?styles.isOn:styles.isOff}
+      onClick={() => {
+        alert("clicked");
+        setIsOn(!isOn);
+      }}
+    >
+      {props.name}  {props.age} vom Christkind 
+    </div>
+  );
+}
