52c9af9923282a08220e89e3b08ab96de176e3c2
Gabriel <gabriel.drissner@student.htldornbirn.at>
2024-01-16 09:31:11 +0100

    get data

--- src/components/PeopleContainer.js
+++ src/components/PeopleContainer.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function PeopleContainer() {
 
   
+useEffect(() => {
+    fetch("https://659fef475023b02bfe8adea7.mockapi.io/person").then(
+        (res) => res.json().then((data) => {
+            console.log(data);  
+
+            
+        })
+    );
+},[]);
+
   return (
     <div>
       <h1>People</h1>
