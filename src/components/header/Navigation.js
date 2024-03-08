0498be9ec3b35cd5f1794e8ad01255f9a4306da6
Gabriel <gabriel.drissner@student.htldornbirn.at>
2024-03-08 15:25:34 +0100

    finished the navigation

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import "./Navigation.css";
+import React, { useState } from "react";
 
-export default function Navigation() {
+const BurgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <ul class="nav">
-    <li><a class="menu__item" href="/">Home</a></li>
-    <li><a class="menu__item" href="/table">Tabelle</a></li>
-    <li><a class="menu__item" href="/matchday">Spieltag</a></li>
-  </ul>
-  )
-}
+    <div className="menu">
+      <input
+        id="menu__toggle"
+        type="checkbox"
+        className="menu__toggle"
+        checked={isOpen}
+        onChange={toggleMenu}
+      />
+      <label className="menu__btn" htmlFor="menu__toggle">
+        <span></span>
+      </label>
+
+      <ul className={`menu__box ${isOpen ? 'open' : ''}`}>
+        <li>
+          <a className="menu__item" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="menu__item" href="/table">
+            Tabelle
+          </a>
+        </li>
+        <li>
+          <a className="menu__item" href="/matchday">
+            Spieltag
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default BurgerMenu;
